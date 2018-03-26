1d0ac1f3355058a134cf9bf937d17a7065244abb
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-03-27 01:46:32 +0600

    PDF generator implementation successfully

--- includes/class.weforms_pdf.php
+++ includes/class.weforms_pdf.php
@@ -1,8 +1,12 @@
 <?php
 
 /**
-* WeForms PDF backend
-*/
+ * Weforms PDF
+ *
+ * Main Class to generate
+ * and download weforms
+ * data as pdf
+ */
 class weforms_pdf {
 	
 	public function __construct() {
@@ -15,6 +19,12 @@ class weforms_pdf {
 		
 	}
 
+	/**
+	 * Callback while activation
+	 * Check if weforms activate
+	 * 
+	 * @return void
+	 */
 	public static function activate() {
 
         if ( ! function_exists( 'weforms' ) ) {
@@ -28,6 +38,13 @@ class weforms_pdf {
 	
 	}
 
+	/**
+	 * Create pdf download link to backend
+	 * 
+	 * @param  int $entries
+	 * @param  int $form_id
+	 * @return string
+	 */
     public function download_link( $entries, $form_id ) {
 
         foreach ($entries as $key => $entry) {
@@ -38,11 +55,141 @@ class weforms_pdf {
         return $entries;
     }
 
+    /**
+     * Add column to entries field
+     * 
+     * @param  string $column 
+     * @param  int $form_id
+     * @return array         
+     */
     public function pdf_column( $column, $form_id ) {
 
         $column['weforms_pdf'] = 'WeForms PDF';
 
         return $column;
+    }
+
+    /**
+     * Prepare pdf for download
+     * 
+     * @return void
+     */
+    public function download_pdf() {
+
+    	require_once plugin_dir_path( __DIR__ ). 'fpdf/fpdf.php';
+
+    	if (!empty($_REQUEST['entry_id'])) {
+
+    		$form_entry_id = $_REQUEST['entry_id'];
+    	
+    	} else {
+    		
+    		$form_entry_id = '';
+    	
+    	}
+
+        global $wpdb;
+
+        $query = 'SELECT form_id FROM ' . $wpdb->weforms_entries . ' WHERE id = %d';
+
+        $form_id = $wpdb->get_var( $wpdb->prepare( $query, $form_entry_id ) );
+
+        $form       = weforms()->form->get($form_id);
+        
+        $entry      = $form->entries()->get($form_entry_id);
+        
+        $fields     = $entry->get_fields();
+        
+        $submission_status = weforms_get_entry( $form_entry_id );
+
+        $user_info = get_userdata($submission_status->user_id);
+
+        $form_name   = get_the_title( $form_id );
+        
+        $header_type = weforms_get_settings( 'pdf_header_type' , 'title' );
+        
+        $logo = weforms_get_settings('pdf_logo');
+        
+        $fpdf = new FPDF();
+        
+        $fpdf->SetMargins(20,20,20);
+        
+        $fpdf->AddPage();
+
+        $fpdf->SetFont('Arial','B',20);
+        $fpdf->SetY(25);
+        $fpdf->Cell(62,10, get_bloginfo( 'name' ), null, null, 'L');
+
+        $fpdf->SetFont('Arial','B',10);
+        $fpdf->SetY(25);
+        $fpdf->SetX(130);
+        $fpdf->Cell(62,10, 'Generated From', null, null, 'R');
+        $fpdf->SetY(29);
+        $fpdf->SetX(130);
+        $fpdf->SetFont('Arial','',8);
+        $fpdf->Cell(62,10, get_bloginfo( 'url' ), null, null, 'R');
+        $fpdf->Ln();
+        $fpdf->Ln();
+        $fpdf->SetFont('Arial','B', 12);
+        $fpdf->Cell(85,10, 'Form Field', 'B');
+        $fpdf->Cell(86,10, 'Field Value', 'B');
+        $fpdf->SetFont('Arial','',14);
+        $fpdf->Ln();
+
+
+        foreach( $fields as $field ) {
+
+            $field_value = $field['value'];
+
+            if (is_array($field_value)) {
+
+                $field_value = implode( ", ", $field_value );
+
+            } elseif ( is_serialized( $field_value ) ) {
+
+                $field_value = implode(',', unserialize( $field_value ));
+
+            }
+
+            $fpdf->Cell(85,10, $field['label']);
+
+            if ( $field['type'] == 'image_upload' ){
+
+                $fpdf->Image($field_value, null, null, 85, 50);
+            
+            } else {
+
+                $fpdf->Write(10, strip_tags($field_value));
+            
+            }
+
+            $fpdf->Ln();
+        }
+
+        $fpdf->Ln();
+        
+        $fpdf->Ln();
+
+        $fpdf->Cell(null,null, null, 'B');
+
+        $fpdf->Ln();
+        
+        $fpdf->Ln();
+
+        $fpdf->SetFont('Arial','B', 10);
+        
+        $fpdf->Cell(85,10, 'Form Name: '.$form_name);
+
+        $fpdf->SetFont('Arial','', 10);
+        
+        $fpdf->Cell(85,10, 'Submitted on: '.date_i18n( 'F j Y, g: i a', strtotime($submission_status->created_at)), null, null, 'R');
+        
+        $fpdf->Ln(5);
+
+        $fpdf->Cell(85,10, 'Submitted by: '.$user_info->user_login);
+
+        $fpdf->output('I', get_the_title( $form_id ).'-'.$form_id.'.pdf');
+
     }
 
 }
